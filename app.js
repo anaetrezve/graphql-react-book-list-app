5147993e8943de90a7724553ea95bb49fb1752ee
Anaet Hossain <anaethoss@gmail.com>
2019-08-12 19:51:51 +0600

    mongoose add & db connected

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const mongoose = require('mongoose');
 const bookSchema = require('./schema/schema');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// connect to mongodb database
+mongoose.connect('mongodb://localhost:27017/graphql-books', {
+  useNewUrlParser: true,
+});
+
+mongoose.connection.once('open', () => console.log('database connected..'));
 
 app.use(
   '/graphql',
